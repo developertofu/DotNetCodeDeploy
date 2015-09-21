4447d58a9f080c3fd23ec876791d640cd2fac461
DeveloperTofu <tofuller30@gmail.com>
2015-09-21 23:54:28 +0000

    live demo ... yikes

--- SQSTester/Program.cs
+++ SQSTester/Program.cs
@@ -69,7 +69,7 @@ namespace SQSTester
                 var sendMessageRequest = new SendMessageRequest
                     {
                         QueueUrl = myQueueUrl, //URL from initial queue creation
-                        MessageBody = string.Format("TEST 2 - This is my message text from server - {0}",System.Environment.MachineName)
+                        MessageBody = string.Format("ATL MEETUP - This is my message text from server - {0}",System.Environment.MachineName)
                     };
                 sqs.SendMessage(sendMessageRequest);
 
