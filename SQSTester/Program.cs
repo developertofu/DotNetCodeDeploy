9cfc41b59e9b96ebbce05a60665f21473243affc
DeveloperTofu <tofuller30@gmail.com>
2015-09-21 18:29:25 +0000

    message changed

--- SQSTester/Program.cs
+++ SQSTester/Program.cs
@@ -69,7 +69,7 @@ namespace SQSTester
                 var sendMessageRequest = new SendMessageRequest
                     {
                         QueueUrl = myQueueUrl, //URL from initial queue creation
-                        MessageBody = string.Format("This is my message text from server - {0}",System.Environment.MachineName)
+                        MessageBody = string.Format("TEST 2 - This is my message text from server - {0}",System.Environment.MachineName)
                     };
                 sqs.SendMessage(sendMessageRequest);
 
