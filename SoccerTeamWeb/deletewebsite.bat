88c28fed381752489fb39c0f20cae4740721f0c8
DeveloperTofu <tofuller30@gmail.com>
2015-09-25 20:56:47 +0000

    fix to remove dir

--- SoccerTeamWeb/deletewebsite.bat
+++ SoccerTeamWeb/deletewebsite.bat
@@ -1,4 +1,3 @@
 cd c:\temp
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\deletewebsite.ps1"
-IF EXIST c:\inetpub\wwwroot\soccerteamweb del c:\inetpub\wwwroot\soccerteamweb
-IF EXIST c:\inetpub\wwwroot\soccerteamweb\bin del c:\inetpub\wwwroot\soccerteamweb\bin
+IF EXIST c:\inetpub\wwwroot\soccerteamweb rmdir /S /Q c:\inetpub\wwwroot\soccerteamweb
