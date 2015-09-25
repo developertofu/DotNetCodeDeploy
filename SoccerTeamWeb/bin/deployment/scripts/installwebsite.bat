854d53b75eb7615fb58be600d69239ee1dc4054a
DeveloperTofu <tofuller30@gmail.com>
2015-09-25 20:15:26 +0000

    fixed name of script

--- SoccerTeamWeb/bin/deployment/scripts/installwebsite.bat
+++ SoccerTeamWeb/bin/deployment/scripts/installwebsite.bat
@@ -1,4 +1,4 @@
 IF NOT EXIST c:\inetpub\wwwroot\soccerteamweb mkdir c:\inetpub\wwwroot\soccerteamweb
 IF NOT EXIST c:\inetpub\wwwroot\soccerteamweb\bin mkdir c:\inetpub\wwwroot\soccerteamweb\bin
 cd c:\temp
-%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\runscript.ps1"
+%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\installwebsite.ps1"
