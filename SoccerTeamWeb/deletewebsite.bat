05bcbb1a5226ef4b3ebea1b04bfbb71bebb0227b
DeveloperTofu <tofuller30@gmail.com>
2015-09-26 01:54:45 +0000

    force deletes

--- SoccerTeamWeb/deletewebsite.bat
+++ SoccerTeamWeb/deletewebsite.bat
@@ -1,3 +1,3 @@
 cd c:\temp
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\deletewebsite.ps1"
-IF EXIST c:\inetpub\wwwroot\soccerteamweb rmdir /S /Q c:\inetpub\wwwroot\soccerteamweb
+IF EXIST c:\inetpub\wwwroot\soccerteamweb rmdir /f /s /q c:\inetpub\wwwroot\soccerteamweb
