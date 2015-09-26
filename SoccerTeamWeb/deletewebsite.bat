0e03f045d583dec65e6e1f344942fd57ad62a9ed
DeveloperTofu <tofuller30@gmail.com>
2015-09-26 02:35:15 +0000

    moved delete site script

--- SoccerTeamWeb/deletewebsite.bat
+++ SoccerTeamWeb/deletewebsite.bat
@@ -1,4 +1,3 @@
-cd c:\temp
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\deletewebsite.ps1"
 IF EXIST c:\inetpub\wwwroot\soccerteamweb\bin rmdir /s /q c:\inetpub\wwwroot\soccerteamweb\bin
 IF EXIST c:\inetpub\wwwroot\soccerteamweb rmdir /s /q c:\inetpub\wwwroot\soccerteamweb
