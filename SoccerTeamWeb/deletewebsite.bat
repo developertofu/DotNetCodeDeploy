c29514f5629bf02851cea92ee906cfe099073922
DeveloperTofu <tofuller30@gmail.com>
2015-09-26 02:02:29 +0000

    trying to delete files

--- SoccerTeamWeb/deletewebsite.bat
+++ SoccerTeamWeb/deletewebsite.bat
@@ -1,3 +1,4 @@
 cd c:\temp
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\deletewebsite.ps1"
-IF EXIST c:\inetpub\wwwroot\soccerteamweb rmdir /f /s /q c:\inetpub\wwwroot\soccerteamweb
+IF EXIST c:\inetpub\wwwroot\soccerteamweb\bin rmdir /s /q c:\inetpub\wwwroot\soccerteamweb\bin
+IF EXIST c:\inetpub\wwwroot\soccerteamweb rmdir /s /q c:\inetpub\wwwroot\soccerteamweb
